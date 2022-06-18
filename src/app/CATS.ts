5a1e74c366e0fc13557695a62e232b912eb8ba12
Olexiy <a1980dominov@gmail.com>
2022-06-18 22:54:04 +0300

    Cats canget

---
+++ src/app/CATS.ts
@@ -0,0 +1,3 @@
+export interface CATS{
+  name:String
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import { AllCatsComponent } from './all-cats/all-cats.component'
+import {FormsModule} from "@angular/forms";
+import { CatsPaginationComponent } from './cats-pagination/cats-pagination.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AllCatsComponent,
+    CatsPaginationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
